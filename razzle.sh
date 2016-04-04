4ffeb11b89546d6182a3e6199fbb9ed85a4f89d2
Chris Orcutt <orcutt.chris@gmail.com>
2016-04-03 21:46:43 -0700

    Initial commit

---
+++ razzle.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if (("$#" == 0)); then
+    path_to_self=$(pwd)
+    for file in $(ls); do
+	if [ -d $file ]; then
+	    cat <<EOF > $file/.gitignore
+*~
+.DS_Store
+EOF
+	    (cd $file; exec $path_to_self/$(basename $0) $path_to_self)
+	fi
+    done
+fi
+
+for file in $(ls); do
+    if [ -d $file ]; then
+	cat <<EOF > $file/.gitignore
+*~
+.DS_Store
+EOF
+	(cd $file; exec $1/$(basename $0) $1)
+    fi
+done
+
+	
